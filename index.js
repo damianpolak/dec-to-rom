03c03e9a168feb5cfaa4ab947982f84a129faa50
damianpolak <damianpolak.it@gmail.com>
2018-07-20 12:23:51 +0200

    Throws + working converter

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
- * decToRom
+ * dec-to-rom
  * https://github.com/damianpolak/dec-to-rom
  *
  * Copyright 2018, Damian Polak
@@ -14,8 +14,32 @@ module.exports = decToRom = (number) => {
 
   if(number === undefined ||
      number === null ||
+     number === 0 ||
      number === '' ||
      typeof(number) !== 'number') {
-       throw new TypeError('Expected correct integer value');
+       throw new TypeError('Expected correct integer value between 1 - 100000');
   }
+
+  number = Math.floor(number);
+  let decValues = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+  let romValues = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M'];
+  let roman = '';
+
+  let rotate = () => {
+    for(let i = decValues.length - 1; i >= 0; --i) {
+      if(number >= decValues[i] && number < 100000) {
+        number -= decValues[i];
+        return romValues[i];
+      } else if(number >= decValues[i-1] && number < decValues[i]) {
+        number -= decValues[i-1];
+        return romValues[i-1];
+      }
+    }
+  }
+
+  while(number != 0) {
+     roman += rotate();
+  }
+
+  return roman;
 };
